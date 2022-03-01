a5b72462783feb0747fd3911f5bba62e6aa2a56c
Shivanidwiet <shivanidwiet@gmail.com>
2022-03-01 11:57:56 +0530

    changes
    changes

---
+++ employer/assets/js/custom.js
@@ -0,0 +1,52 @@
+
+
+$(document).ready(function(){
+$('.users').owlCarousel({
+    loop:true,
+	center: false,
+	autoplay:true,
+    margin:10,
+    nav:true,
+	dots: true,
+	autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:10
+        }
+    }
+});
+});
+
+
+ $(document).ready(function () {
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+		$(this).toggleClass('active');
+	});
+});
+
+
+
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+function countTimer() {
+           ++totalSeconds;
+           var hour = Math.floor(totalSeconds /3600);
+           var minute = Math.floor((totalSeconds - hour*3600)/60);
+           var seconds = totalSeconds - (hour*3600 + minute*60);
+           if(hour < 10)
+             hour = "0"+hour;
+           if(minute < 10)
+             minute = "0"+minute;
+           if(seconds < 10)
+             seconds = "0"+seconds;
+           document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+        }
+
