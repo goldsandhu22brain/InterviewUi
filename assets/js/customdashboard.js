02fd114106df47b1c7f198257e16968bbeedb86e
Shivanidwiet <shivanidwiet@gmail.com>
2022-03-28 11:23:29 +0530

    Story Page
    Changes

--- assets/js/customdashboard.js
+++ assets/js/customdashboard.js
@@ -85,27 +85,24 @@ $(document).ready(function(){
 
 
 
-
-
+ if ( document.getElementById("timer") != null ) {
 
 var timerVar = setInterval(countTimer, 1000);
 var totalSeconds = 0;
 function countTimer() {
-           ++totalSeconds;
-           var hour = Math.floor(totalSeconds /3600);
-           var minute = Math.floor((totalSeconds - hour*3600)/60);
-           var seconds = totalSeconds - (hour*3600 + minute*60);
-           if(hour < 10)
-             hour = "0"+hour;
-           if(minute < 10)
-             minute = "0"+minute;
-           if(seconds < 10)
-             seconds = "0"+seconds;
-           document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
-        }
-
-
-
+       ++totalSeconds;
+       var hour = Math.floor(totalSeconds /3600);
+       var minute = Math.floor((totalSeconds - hour*3600)/60);
+       var seconds = totalSeconds - (hour*3600 + minute*60);
+       if(hour < 10)
+         hour = "0"+hour;
+       if(minute < 10)
+         minute = "0"+minute;
+       if(seconds < 10)
+         seconds = "0"+seconds;
+       document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+    }
+}
 
 
 
