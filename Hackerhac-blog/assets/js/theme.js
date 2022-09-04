cf30dee1be88c991e8607d3e2fe760146dc2a44f
Saravanaind <asharavanakumar@gmail.com>
2022-09-04 17:44:41 +0530

    b

--- Hackerhac-blog/assets/js/theme.js
+++ Hackerhac-blog/assets/js/theme.js
@@ -151,3 +151,10 @@ jQuery(document).ready(function($){
 
 
 });
+var thumbTitle = $(".thumb-title");
+for (var i = 0; i<= thumbTitle.length; i++){
+    if(thumbTitle[i].innerHTML.length > 50){
+        var shortendTitle =thumbTitle[i].innerHTML.slice(0, 50);
+        thumbTitle[i].innerHTML = shortendTitle.concat("...");
+    }
+}
