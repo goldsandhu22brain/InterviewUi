d5264a8b4721f4f973066ef25a9cd865d3453a3a
developer-kgetechnologies-HPSSD <developer@kgetechnologies.com>
2024-04-04 17:32:05 +0530

    cleanup

--- Dashboard/script.js
+++
@@ -1,1 +0,0 @@
-// Nope. This is a concept design, so no menu or filter toggles work. But it looks good, doesn't it?
--- old/Scripts/dummy.js
+++
@@ -1,2 +0,0 @@
-
-//Write a function to Show popup that you are not authorized to download the file
